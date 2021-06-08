6255e436d747cd09efc540a1c013413212eea0a1
George Kettle <georgekettle.gk@gmail.com>
2021-06-08 19:44:07 +1000

    search filter JS controller fixed (no position absolute now

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -5,15 +5,7 @@ export default class extends Controller {
   static targets = ["search", "container"]
 
   initialize() {
-    this.initShuffle();
     this.hasSearchTarget && this.initSearch();
-  }
-
-  initShuffle() {
-    this.shuffle = new Shuffle(this.containerTarget, {
-      itemSelector: '.filter-item',
-      sizer: '.filter-sizer-element',
-    });
   }
 
   initSearch() {
@@ -24,10 +16,49 @@ export default class extends Controller {
   }
 
   applyFilter(query) {
-    this.shuffle.filter(function (element, shuffle) {
-      var elementText = element.dataset.filterText.toLowerCase();
-
-      return elementText.indexOf(query) !== -1;
-    });
+    // const selectFilterClasses = this.inclusiveFiltersValue.length ? this.inclusiveFiltersValue.map(inclusiveFilter => `:scope > ${filterClasses}${inclusiveFilter}`).join(',') : filterClasses;
+    // const deselectFilterClasses = this.inclusiveFiltersValue.length ? this.inclusiveFiltersValue.map(inclusiveFilter => `:scope > div:not(${filterClasses}${inclusiveFilter})`).join(',') : `:scope > div:not(${filterClasses})`;
+    const childDivs = Array.from(this.containerTarget.children).filter(elem => elem.tagName === 'DIV')
+    childDivs.forEach((elem) => {
+      var elemText = elem.dataset.filterText;
+      if (elemText) {
+        this.isQueryMatch(elemText, query) && this.showElement(elem)
+        !this.isQueryMatch(elemText, query) && this.hideElement(elem)
+      }
+    })
   }
+
+  isQueryMatch(text, query) {
+    return text.toLowerCase().indexOf(query) !== -1
+  }
+
+
+  showElement(elem) {
+    if (elem.classList.contains('fadeOutShrink')) {
+      elem.classList.add('animated','fadeInExpand');
+      elem.classList.remove('fadeOutShrink');
+    }
+  }
+
+  hideElement(elem) {
+    elem.classList.add('animated','fadeOutShrink');
+    elem.classList.remove('fadeInExpand');
+  }
+
+
+  // applyFilter() {
+  //   const filterClasses = this.filtersValue.length ? this.filtersValue.join('') : '*';
+  //   const selectFilterClasses = this.inclusiveFiltersValue.length ? this.inclusiveFiltersValue.map(inclusiveFilter => `:scope > ${filterClasses}${inclusiveFilter}`).join(',') : filterClasses;
+  //   const deselectFilterClasses = this.inclusiveFiltersValue.length ? this.inclusiveFiltersValue.map(inclusiveFilter => `:scope > div:not(${filterClasses}${inclusiveFilter})`).join(',') : `:scope > div:not(${filterClasses})`;
+
+  //   if (deselectFilterClasses.length) {
+  //     let deselected = this.parentTarget.querySelectorAll(deselectFilterClasses);
+  //     this.hideElements(deselected);
+  //   }
+
+  //   if (selectFilterClasses.length) {
+  //     let selected = this.parentTarget.querySelectorAll(selectFilterClasses);
+  //     this.showElements(selected);
+  //   }
+  // }
 }
