f440cc2a631d1861f0c008261bcfba0786b8bfc4
George Kettle <georgekettle.gk@gmail.com>
2021-06-08 19:55:04 +1000

    remove shuffle import

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -1,5 +1,4 @@
 import { Controller } from "stimulus"
-import Shuffle from 'shufflejs'
 
 export default class extends Controller {
   static targets = ["search", "container"]
@@ -16,8 +15,6 @@ export default class extends Controller {
   }
 
   applyFilter(query) {
-    // const selectFilterClasses = this.inclusiveFiltersValue.length ? this.inclusiveFiltersValue.map(inclusiveFilter => `:scope > ${filterClasses}${inclusiveFilter}`).join(',') : filterClasses;
-    // const deselectFilterClasses = this.inclusiveFiltersValue.length ? this.inclusiveFiltersValue.map(inclusiveFilter => `:scope > div:not(${filterClasses}${inclusiveFilter})`).join(',') : `:scope > div:not(${filterClasses})`;
     const childDivs = Array.from(this.containerTarget.children).filter(elem => elem.tagName === 'DIV')
     childDivs.forEach((elem) => {
       var elemText = elem.dataset.filterText;
@@ -44,21 +41,4 @@ export default class extends Controller {
     elem.classList.add('animated','fadeOutShrink');
     elem.classList.remove('fadeInExpand');
   }
-
-
-  // applyFilter() {
-  //   const filterClasses = this.filtersValue.length ? this.filtersValue.join('') : '*';
-  //   const selectFilterClasses = this.inclusiveFiltersValue.length ? this.inclusiveFiltersValue.map(inclusiveFilter => `:scope > ${filterClasses}${inclusiveFilter}`).join(',') : filterClasses;
-  //   const deselectFilterClasses = this.inclusiveFiltersValue.length ? this.inclusiveFiltersValue.map(inclusiveFilter => `:scope > div:not(${filterClasses}${inclusiveFilter})`).join(',') : `:scope > div:not(${filterClasses})`;
-
-  //   if (deselectFilterClasses.length) {
-  //     let deselected = this.parentTarget.querySelectorAll(deselectFilterClasses);
-  //     this.hideElements(deselected);
-  //   }
-
-  //   if (selectFilterClasses.length) {
-  //     let selected = this.parentTarget.querySelectorAll(selectFilterClasses);
-  //     this.showElements(selected);
-  //   }
-  // }
 }
