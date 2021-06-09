ebdd290d4e1b676b394fde1e5714e6886258d098
George Kettle <georgekettle.gk@gmail.com>
2021-06-09 14:07:38 +1000

    reorder of channel list items when new messages created

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -7,7 +7,9 @@ class Notification < ApplicationRecord
   after_create_commit {
     if type == 'MessageNotification'
       # channel list item (remove and then prepend)
-      broadcast_replace_to(recipient, params[:message].channel, :channel_list_item, target: "channel_list_item_#{params[:message].channel.id}", partial: "components/lists/channel_list_item", locals: { channel: params[:message].channel, unread_messages: true })
+      # broadcast_prepend_to(recipient, params[:message].channel, :channel_list_item, target: "channel_list_items", partial: "components/lists/channel_list_item", locals: { channel: params[:message].channel, unread_messages: true })
+      # broadcast_remove_to(recipient, params[:message].channel, :channel_list_item)
+      broadcast_replace_to(recipient, params[:message].channel, :channel_list_item, target: "channel_list_item_#{params[:message].channel.id}", partial: "components/lists/channel_list_item", locals: { channel: params[:message].channel, unread_messages: true, move_to_top: true })
       # navbar channels button
       broadcast_replace_to(recipient, :channels_button, target: "channels_button", partial: "shared/navbar/channels_button", locals: { badge: true })
     else
