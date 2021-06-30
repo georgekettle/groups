217253691b72e3fc08e16e5fef418c0e4ab88dd9
George Kettle <georgekettle.gk@gmail.com>
2021-06-30 21:03:20 +1000

    policy added to messages

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,6 @@
 class NotificationsController < ApplicationController
+  skip_after_action :verify_policy_scoped, only: [:index]
+
   def index
     current_user.profile.update(notifications_checked: true)
     @notifications = current_user.profile.notifications.newest_first.where(type: 'MentionNotification')
