90a5961e3458e7811bfaad4c1e84e43f6aac08e6
George Kettle <georgekettle.gk@gmail.com>
2021-07-05 13:03:23 +1000

    prompting for notifications based upon whether user has been asked before

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_03_095853) do
+ActiveRecord::Schema.define(version: 2021_07_05_010216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -124,6 +124,7 @@ ActiveRecord::Schema.define(version: 2021_07_03_095853) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false, null: false
     t.string "expo_token"
+    t.boolean "ask_to_accept_notifications", default: true, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
